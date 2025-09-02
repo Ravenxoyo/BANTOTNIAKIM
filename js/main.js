220ae43c7b35028cf2129fa757c77d2652c0e773
Zen <156103519+Ravenxoyo@users.noreply.github.com>
2025-09-02 22:36:29 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HI BABY HAPPY GF DAY PO ILOVEYOUUUUUUUU').split('')
+    const titles = ('FOR YOU XYYY :>>').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
