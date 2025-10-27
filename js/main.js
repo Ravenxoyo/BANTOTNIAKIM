1825e52d69d2260f84beef0468d1119142f2cd08
Zen <156103519+Ravenxoyo@users.noreply.github.com>
2025-10-27 21:29:31 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('FOR YOU XYYY :>>').split('')
+    const titles = ('BANTOT TALAGA NI MIKA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
